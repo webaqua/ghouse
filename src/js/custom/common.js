f944514abd4ddb53a552e271ffd7f116c1375d42
vizalerd <vizalerd@gmail.com>
2017-03-07 16:07:12 +0600

    one page scroll done

--- src/js/custom/common.js
+++ src/js/custom/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $(".main").onepage_scroll({
-     sectionContainer: "container",     // sectionContainer accepts any kind of selector in case you don't want to use section
+     sectionContainer: "article",     // sectionContainer accepts any kind of selector in case you don't want to use section
      easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
                                       // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
      animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
