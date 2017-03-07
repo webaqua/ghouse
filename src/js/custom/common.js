cdcbe976cff9470694ba00449491cfe67cff12bd
webaqua <s-h-group@yandex.ru>
2017-03-07 15:26:22 +0600

    fullpage scroll in  progress

--- src/js/custom/common.js
+++ src/js/custom/common.js
@@ -1,90 +1,109 @@
 $(document).ready(function(){
+
+  $(".main").onepage_scroll({
+     sectionContainer: "container",     // sectionContainer accepts any kind of selector in case you don't want to use section
+     easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                      // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+     animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+     pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+     updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+     beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+     afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+     loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+     keyboard: true,                  // You can activate the keyboard controls
+     responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                      // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                      // the browser's width is less than 600, the fallback will kick in.
+     direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
+  });
+
+
   $(".js-vertical-tab-content").hide();
-$(".js-vertical-tab-content:first").show();
+  $(".js-vertical-tab-content:first").show();
 
-/* if in tab mode */
+  /* if in tab mode */
 
-$(".js-vertical-tab").click(function(event) {
-event.preventDefault();
+  $(".js-vertical-tab").click(function(event) {
+  event.preventDefault();
 
-$(".js-vertical-tab-content").hide();
-var activeTab = $(this).attr("rel");
-$("#"+activeTab).show();
+  $(".js-vertical-tab-content").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).show();
 
-$(".js-vertical-tab").removeClass("is-active");
-$(this).addClass("is-active");
+  $(".js-vertical-tab").removeClass("is-active");
+  $(this).addClass("is-active");
 
-$(".js-vertical-tab-accordion-heading").removeClass("is-active");
-$(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
-});
+  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+  $(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
+  });
 
-/* if in accordion mode */
+  /* if in accordion mode */
 
-$(".js-vertical-tab-accordion-heading").click(function(event) {
-event.preventDefault();
+  $(".js-vertical-tab-accordion-heading").click(function(event) {
+  event.preventDefault();
 
-$(".js-vertical-tab-content").hide();
-var accordion_activeTab = $(this).attr("rel");
-$("#"+accordion_activeTab).show();
+  $(".js-vertical-tab-content").hide();
+  var accordion_activeTab = $(this).attr("rel");
+  $("#"+accordion_activeTab).show();
 
-$(".js-vertical-tab-accordion-heading").removeClass("is-active");
-$(this).addClass("is-active");
+  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+  $(this).addClass("is-active");
 
-$(".js-vertical-tab").removeClass("is-active");
-$(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
-});
+  $(".js-vertical-tab").removeClass("is-active");
+  $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+  });
 
 
-$(window).resize(function() {
-  var more = document.getElementById("js-navigation-more");
-  if ($(more).length > 0) {
-    var windowWidth = $(window).width();
-    var moreLeftSideToPageLeftSide = $(more).offset().left;
-    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+  $(window).resize(function() {
+    var more = document.getElementById("js-navigation-more");
+    if ($(more).length > 0) {
+      var windowWidth = $(window).width();
+      var moreLeftSideToPageLeftSide = $(more).offset().left;
+      var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
 
-    if (moreLeftSideToPageRightSide < 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
-    }
-
-    if (moreLeftSideToPageRightSide > 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
-    }
-  }
-});
-
-$(document).ready(function() {
-  var menuToggle = $("#js-mobile-menu").unbind();
-  $("#js-navigation-menu").removeClass("show");
-
-  menuToggle.on("click", function(e) {
-    e.preventDefault();
-    $("#js-navigation-menu").slideToggle(function(){
-      if($("#js-navigation-menu").is(":hidden")) {
-        $("#js-navigation-menu").removeAttr("style");
+      if (moreLeftSideToPageRightSide < 330) {
+        $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+        $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
       }
+
+      if (moreLeftSideToPageRightSide > 330) {
+        $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+        $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+      }
+    }
+  });
+
+  $(document).ready(function() {
+    var menuToggle = $("#js-mobile-menu").unbind();
+    $("#js-navigation-menu").removeClass("show");
+
+    menuToggle.on("click", function(e) {
+      e.preventDefault();
+      $("#js-navigation-menu").slideToggle(function(){
+        if($("#js-navigation-menu").is(":hidden")) {
+          $("#js-navigation-menu").removeAttr("style");
+        }
+      });
     });
   });
-});
 
 
-$(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
+  $(function() {
+    $("#modal-1").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
   });
-
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
-});
 
             })
